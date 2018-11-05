4fc3d649a38037c5d241f395871c5bb9f0e2ba10
hevienz <hevienz@qq.com>
2018-11-05 21:21:53 +0800

    release v1.2.7

---
+++ stuff-in-kvm/opt/common/bin/monitor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import httplib
+import json
+import socket
+import sys
+
+conn = httplib.HTTPConnection("127.0.0.1:9200", timeout=10)
+try:
+    conn.request("GET", "/_cluster/stats")
+except socket.error as e:
+    sys.exit(1)
+r = conn.getresponse()
+data = r.read()
+d = json.loads(data)
+
+nodes = d['nodes']
+jvm = nodes['jvm']
+jvm_mem = jvm['mem']
+
+cluster_jvm_heap_used_in_percent = float(jvm_mem['heap_used_in_bytes']) / float(jvm_mem['heap_max_in_bytes'])
+
+indices = d['indices']
+shards = indices['shards']
+
+docs = indices['docs']
+
+output = {
+    'cluster_status': d['status'],
+    'cluster_jvm_heap_used_in_percent': int(cluster_jvm_heap_used_in_percent * 100 * 10),
+    'cluster_jvm_threads_count': int(jvm['threads']),
+    'cluster_indices_count': int(indices['count']),
+    'cluster_shards_primaries_count': int(shards['primaries']),
+    'cluster_shards_replication_count': int(shards['replication']),
+    'cluster_docs_count': int(docs['count']),
+    'cluster_docs_deleted_count': int(docs['deleted']),
+}
+
+try:
+    conn.request("GET", "/_cluster/health")
+except socket.error as e:
+    sys.exit(1)
+r = conn.getresponse()
+data = r.read()
+d = json.loads(data)
+
+output.update(d)
+
+print(json.dumps(output))
--- stuff-in-kvm/opt/common/bin/monitor.sh
+++ stuff-in-kvm/opt/common/bin/monitor.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-. /opt/common/bin/func.sh
-
-CTRID=`ctrid $1`
-
-IMAGE_NAME=`echo $1 | awk -F: '{print $1}'`
-
-[ -n "${IMAGE_NAME}" ] && docker exec $CTRID /opt/${IMAGE_NAME}/bin/monitor.sh
+exec python /opt/common/bin/monitor.py
